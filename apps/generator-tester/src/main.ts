650b2f53e2249cfe335f03f9ec8997f24117eea5
Steven Goff <steveng9@gmail.com>
2022-06-01 09:59:36 -0500

    feat: consume utils-generator from generator-tester
    run the example via `nx run generator-tester:serve`

--- apps/generator-tester/src/main.ts
+++ apps/generator-tester/src/main.ts
@@ -1,1 +1,3 @@
-console.log('Hello World!');
+import { promptCharles } from '@caribou-crew/mezzo-utils-generator';
+
+promptCharles();
--- libs/utils-generator/src/lib/utils-generator.ts
+++ libs/utils-generator/src/lib/utils-generator.ts
@@ -1,3 +1,30 @@
-export function utilsGenerator(): string {
-  return 'utils-generator';
+import * as inquirer from 'inquirer';
+
+export async function promptCharles() {
+  const answer = await inquirer.prompt([
+    {
+      name: 'useCharlesFile',
+      type: 'list',
+      message: 'Want to use a Charles file?',
+      choices: [
+        { name: 'Yes', value: true },
+        { name: 'No', value: false },
+      ],
+    },
+    {
+      name: 'charlesFileExtension',
+      type: 'input',
+      message: '[example prompt] what is the charles extension?',
+      filter: (value) => value.replace(/[']+/g, ''),
+      when: (answers) => answers.useCharlesFile === true,
+      validate: async (charlesPath) => {
+        if (!charlesPath.includes('.chlsj')) {
+          return 'Charles json export must end in .chlsj';
+        }
+        return true;
+      },
+    },
+  ]);
+
+  console.log('got my answers: ', answer);
 }
